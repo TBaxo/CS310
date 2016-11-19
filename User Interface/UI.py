25435ff1db8deb9f383b9a175723db4c7a748672
Thomas Baxendale <tombaxendale95@gmail.com>
2016-11-19 18:45:53 +0000

    Integrated Parser Output with User interface
    Added the parser to the imports for the UI and integrated it's use with
    the parserto retrieve wanted outputs.
    Also added a scrolled text area for use in creating the text editor part
    of the program

--- User Interface/UI.py
+++ User Interface/UI.py
@@ -1,42 +1,52 @@
 from tkinter import *
+from tkinter.scrolledtext import ScrolledText
 import sys
 sys.path.append(r"C:\Users\Thoma\Documents\GitHub\CS310")
-from Compiler import *
-from Parser import *
+from Compiler.compiler import *
+from Parser.parser import Parser
+print(dir())
 class Application (Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
-
+        self.prev_parse = False
         self.master = master
-        self.text_exists = False
         self.init_window()
         
     def init_window(self):
         self.master.title("Haskell Frontend Developer")
         self.pack(fill=BOTH, expand=1)
-        #code for adding menu bar, will most likely replace button
         menu = Menu(self.master)
         self.master.config(menu=menu)
         file = Menu(menu)
-        #file.add_command(label)
         file.add_command(label="Parse New File", command=self.parse_file)
         file.add_command(label="Exit", command=self.client_exit)
         menu.add_cascade(label="File", menu=file)
+        self.textarea = ScrolledText(self)
+        self.textarea.pack(fill=BOTH, expand=1)
+        self.bottomframe = Frame(self, bd=3, relief="sunken")
+        self.bottomframe.pack(side=BOTTOM, fill=X)
         
     def parse_file(self):
         from tkinter import filedialog
         dialog = filedialog.LoadFileDialog(self.master)
         dialog.top.geometry("900x500")
         filename = dialog.go()
-        if not filename == None:
-            if self.text_exists:
-                pass
-            else:
-                self.text = Label(self, text="File Successfully Parsed!")
-                self.text.pack()
-                self.text_exists = True
+        if filename == None:
+            return None
         else:
-            self.text.destroy()
+            p = Parser(filename)
+            p.start_parse()
+            if self.prev_parse:
+                self.bottomframe.data_types.destroy()
+                self.bottomframe.constructors.destroy()
+                self.bottomframe.functions.destroy()
+            self.bottomframe.data_types = Label(self.bottomframe, text="Data Types: " + str(p.data_types))
+            self.bottomframe.constructors = Label(self.bottomframe, text="Constuctors: " + str(p.constructors))
+            self.bottomframe.functions = Label(self.bottomframe, text="Functions: " + str(p.functions))
+            self.bottomframe.data_types.pack()
+            self.bottomframe.constructors.pack()
+            self.bottomframe.functions.pack()
+            self.prev_parse = True
     
     def client_exit(self):
         exit()
